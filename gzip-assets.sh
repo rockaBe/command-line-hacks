f837e4664be66fcf263e75b3a0bc666ada911853
Bernd Nähler <rockaBe@users.noreply.github.com>
2018-12-20 12:39:28 +0100

    add gzip assets script

---
+++ gzip-assets.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Usage:
+# $ ./gzip-assets.sh
+
+mkdir gzipped
+for file in $(find . -type f -depth 1 | egrep "\.(css|js|eot|svg|ttf)$") ; do
+  gzip -c --best $file > gzipped/$file
+done
